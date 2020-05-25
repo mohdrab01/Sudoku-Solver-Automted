dc4dfceec596eeacb4b5be1abae3cfcf78f45ec1
AR <63246629+mohdrab01@users.noreply.github.com>
2020-05-25 13:06:47 +0530

    Update solveSudoku.py

--- solveSudoku.py
+++ solveSudoku.py
@@ -4,20 +4,7 @@ pygame.font.init()
 
 '''SUDOKU DESC'''
 class Sudoku:
-    ''' ENTER UR SUDOKU PRE-NUMBERES HERE and REPLACE BLANKS WITH 0's  '''
-
-    # board = [
-    #     [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    #     [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    #     [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    #     [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    #     [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    #     [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    #     [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    #     [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    #     [0, 4, 9, 2, 0, 6, 0, 0, 7]
-    # ]
-    
+    ''' ENTER UR SUDOKU PRE-NUMBERES HERE and REPLACE BLANKS WITH 0's  '''    
     board = [
         [5, 0, 9, 0, 0, 0, 4, 0, 0],
         [7, 0, 8, 3, 0, 4, 9, 0, 0],
